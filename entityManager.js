805910b8704670b6df45d31aca0451608da6e467
KetillG <ketillgudmunds+github@gmail.com>
2017-10-25 09:47:25 +0000

    Added base from pat to build on

---
+++ entityManager.js
@@ -0,0 +1,74 @@
+/*
+
+entityManager.js
+
+A module which handles arbitrary entity-management for "Asteroids"
+
+
+We create this module as a single global object, and initialise it
+with suitable 'data' and 'methods'.
+
+"Private" properties are denoted by an underscore prefix convention.
+
+*/
+
+
+"use strict";
+
+
+// Tell jslint not to complain about my use of underscore prefixes (nomen),
+// my flattening of some indentation (white), or my use of incr/decr ops 
+// (plusplus).
+//
+/*jslint nomen: true, white: true, plusplus: true*/
+
+
+var entityManager = {
+
+deferredSetup : function () {
+    this._categories = [];
+},
+
+init: function() {
+},
+
+update: function(du) {
+
+    for (var c = 0; c < this._categories.length; ++c) {
+
+        var aCategory = this._categories[c];
+        var i = 0;
+
+        while (i < aCategory.length) {
+
+            var status = aCategory[i].update(du);
+
+            i++;
+        }
+    }
+
+},
+
+render: function(ctx) {
+
+    var debugX = 10, debugY = 100;
+
+    for (var c = 0; c < this._categories.length; ++c) {
+
+        var aCategory = this._categories[c];
+
+        for (var i = 0; i < aCategory.length; ++i) {
+
+            aCategory[i].render(ctx);
+            //debug.text(".", debugX + i * 10, debugY);
+
+        }
+        debugY += 10;
+    }
+}
+
+}
+
+// Some deferred setup which needs the object to have been created first
+entityManager.deferredSetup();
+
---
+++ util.js
@@ -0,0 +1,11 @@
+// util.js
+//
+// A module of utility functions, with no private elements to hide.
+// An easy case; just return an object containing the public stuff.
+
+"use strict";
+
+
+var util = {
+
+};
