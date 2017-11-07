fa63674e11fbdf1b940a16f98e54f7d7c15d0034
Sindri Petur Laptop <sindripetur@gmail.com>
2017-11-07 16:44:02 +0000

    lint revert proper

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  extends: 'airbnb-base',
-  env: {
-    browser: true,
-  },
-  rules: {
-    'class-methods-use-this': 0
-  }
-};
+    extends: 'airbnb-base',
+    env: {
+      browser: true,
+    },
+    rules: {
+      'class-methods-use-this': 0,
+      'linebreak-style': 0,
+      'indent': ['error', 4]
+    }
+  };
