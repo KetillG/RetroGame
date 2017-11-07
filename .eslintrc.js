5adb3720349828d5799bb85dbf2869d54ec8c97c
Sindri Petur Laptop <sindripetur@gmail.com>
2017-11-07 16:55:28 +0000

    eslintmoar

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,12 @@ module.exports = {
     rules: {
       'class-methods-use-this': 0,
       'linebreak-style': 0,
-      'indent': ['error', 4]
+      'indent': ['error', 4],
+      'no-var': 0,
+      'vars-on-top': 0,
+      'guard-for-in': 0,
+      'no-restricted-syntax': 0,
+      'func-names': 0,
+      'block-scoped-var': 0
     }
   };
