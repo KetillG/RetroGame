7795cdb0b90bed2762b35c501f73e9d4a650bbb8
stellarutg <stellarut95@gmail.com>
2017-11-20 18:24:51 +0000

    font fixed when player dies

--- mainloop/scoreboard.js
+++ mainloop/scoreboard.js
@@ -7,33 +7,36 @@ var scoreboard = {
 
 
     start: function (players) {
-        this._players = players;      
+        this._players = players;
         for (let index = 0; index < 4; index++) {
             const domItem = this._playersDOM[index]
             domItem.style.display = 'flex';
             if(!players[index]) {
                 domItem.style.display = 'none';
-            }            
+            }
         }
     },
 
     update: function (du) {
         const MAX_PLAYERS = 4;
         for (let index = 0; index < MAX_PLAYERS; index++) {
-            if(this._players[index]) {
-                //
+            const STAT_COUNT = 4;
+            const statBoxes = this._playersDOM[index].querySelector('.player-score').children;
 
+            if(this._players[index]) {
                 const canvasP = this._playersDOM[index].querySelector('.playerCTX');
                 const ctxP = canvasP.getContext("2d");
                 this.drawPlayer(this._players[index], canvasP, ctxP,index);
 
                 const stats = Object.values(this._players[index].getStats());
-                const statBoxes = this._playersDOM[index].querySelector('.player-score').children;
-                const STAT_COUNT = 4;
+                //const statBoxes = this._playersDOM[index].querySelector('.player-score').children;
                 for (let i = 0; i < STAT_COUNT; i++) {
                     statBoxes[i].children[1].innerHTML = stats[i];
-                    statBoxes[i].children[1].style.fontSize = consts.SCOREBOARD_FONT_SIZE;
+                    //statBoxes[i].children[1].style.fontSize = consts.SCOREBOARD_FONT_SIZE;
                 }
+            }
+            for (let i = 0; i < STAT_COUNT; i++) {
+                statBoxes[i].children[1].style.fontSize = consts.SCOREBOARD_FONT_SIZE;
             }
         }
     },
